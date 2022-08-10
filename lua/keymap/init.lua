12ba2f1949e0643d0d5a61543dff69439085e34d
N.O.Atomusk <orzkey@outlook.com>
2022-08-11 04:22:20 +0800

    格式化

--- lua/keymap/init.lua
+++ lua/keymap/init.lua
@@ -13,34 +13,36 @@ local cmd,cmds = key.cmd, key.cmds
 -- usage of plugins
 nmap({
   -- packer
-  { '<Leader>p', '<Nop>', opts(noremap,silent)},
-  { '<Leader>pu', cmd('PackerUpdate'), opts(noremap, silent) },
-  { '<Leader>pi', cmd('PackerInstall'), opts(noremap, silent) },
-  { '<Leader>pc', cmd('PackerCompile'), opts(noremap, silent) },
+  { '<Leader>p'      , '<Nop>'                     , opts(noremap , silent)}  ,
+  { '<Leader>pu'     , cmd('PackerUpdate')         , opts(noremap , silent) } ,
+  { '<Leader>pi'     , cmd('PackerInstall')        , opts(noremap , silent) } ,
+  { '<Leader>pc'     , cmd('PackerCompile')        , opts(noremap , silent) } ,
   -- dashboard
-  { '<Leader>n', cmd('DashboardNewFile'), opts(noremap, silent) },
-  { '<Leader>s', '<Nop>', opts(noremap,silent)},
-  { '<Leader>ss', cmd('SessionSave'), opts(noremap, silent) },
-  { '<Leader>sl', cmd('SessionLoad'), opts(noremap, silent) },
+  { '<Leader>n'      , cmd('DashboardNewFile')     , opts(noremap , silent) } ,
+  { '<Leader>s'      , '<Nop>'                     , opts(noremap , silent)}  ,
+  { '<Leader>ss'     , cmd('SessionSave')          , opts(noremap , silent) } ,
+  { '<Leader>sl'     , cmd('SessionLoad')          , opts(noremap , silent) } ,
   -- nvimtree
-  { 'tt', cmd('NvimTreeToggle'), opts(noremap, silent) },
+  { 'tt'             , cmd('NvimTreeToggle')       , opts(noremap , silent) } ,
   -- Telescope
-  { '<Leader>b', cmd('Telescope buffers'), opts(noremap, silent) },
-  { '<Leader>fa', cmd('Telescope live_grep'), opts(noremap, silent) },
-  { '<Leader>ff', cmd('Telescope find_files'), opts(noremap, silent) },
+  { '<Leader>b'      , cmd('Telescope buffers')    , opts(noremap , silent) } ,
+  { '<Leader>fa'     , cmd('Telescope live_grep')  , opts(noremap , silent) } ,
+  { '<Leader>ff'     , cmd('Telescope find_files') , opts(noremap , silent) } ,
   -- bufferline.nvim
-  { 'e', cmd('BufferLineCyclePrev'), opts(noremap, silent)},
-  { 'r', cmd('BufferLineCycleNext'), opts(noremap, silent)},
-  { 'gb', cmd('BufferLinePick'), opts(noremap, silent)},
-  { 'tv', cmd('BufferLinePickVsplit'), opts(noremap, silent)},
-  { 'tc', cmd('BufferLinePickClose'), opts(noremap, silent)},
-  { 'tl', cmd('BufferLineMoveNext'), opts(noremap, silent)},
-  { 'th', cmd('BufferLineMovePrev'), opts(noremap, silent)},
+  { 'e'              , cmd('BufferLineCyclePrev')  , opts(noremap , silent)}  ,
+  { 'r'              , cmd('BufferLineCycleNext')  , opts(noremap , silent)}  ,
+  { 'gb'             , cmd('BufferLinePick')       , opts(noremap , silent)}  ,
+  { 'tv'             , cmd('BufferLinePickVsplit') , opts(noremap , silent)}  ,
+  { 'tc'             , cmd('BufferLinePickClose')  , opts(noremap , silent)}  ,
+  { 'tl'             , cmd('BufferLineMoveNext')   , opts(noremap , silent)}  ,
+  { 'th'             , cmd('BufferLineMovePrev')   , opts(noremap , silent)}  ,
   -- nvim-comment
-  { '<C-_>', cmd('CommentToggle'), opts(noremap,silent)},
+  { '<C-_>'          , cmd('CommentToggle')        , opts(noremap , silent)}  ,
 })
 
+-- nvim-comment
 vmap({
-    -- nvim-comment
-    { '<C-_>', cmd('CommentToggle'), opts(noremap,silent)},
+    {'<C-_>'      , [[:CommentToggle<Cr>]] , opts(noremap   , silent)} ,
+    { '<Leader>t' , [[:Tabularize /]]      , opts(noremap)} ,
 })
+

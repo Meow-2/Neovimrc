8dc1e4d804d1f17636ed8900b58a188c064b842e
N.O.Atomusk <orzkey@outlook.com>
2022-08-15 02:40:20 +0800

    add textobj and incremental_selection

--- lua/modules/lang/nvim-treesitter.lua
+++ lua/modules/lang/nvim-treesitter.lua
@@ -11,18 +11,29 @@ return function()
       select = {
         enable = true,
         keymaps = {
-          ['f'] = '@function.outer',
+          ['af'] = '@function.outer',
           ['if'] = '@function.inner',
           ['ac'] = '@class.outer',
           ['ic'] = '@class.inner',
+          ['ap'] = '@parameter.outer',
+          ['ip'] = '@parameter.inner',
         },
       },
+      include_surrounding_whitespace = false,
     },
-
     matchup = {
         enable = true,
         disable = {},
     },
+    incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<Cr>",
+      node_incremental = "<Cr>",
+      node_decremental = "<Bs>",
+      -- scope_incremental = "grc",
+    },
+  },
   })
 end
 

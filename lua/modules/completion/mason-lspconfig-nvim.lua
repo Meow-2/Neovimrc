dd60cd69b23b0a99328c888c47a815e7a5e85c5b
N.O.Atomusk <orzkey@outlook.com>
2022-08-16 02:06:23 +0800

    add bashls

--- lua/modules/completion/mason-lspconfig-nvim.lua
+++ lua/modules/completion/mason-lspconfig-nvim.lua
@@ -1,6 +1,6 @@
 return function()
     require("mason-lspconfig").setup({
-        ensure_installed = { "sumneko_lua","clangd", "cmake", "pyright", "gopls","jsonls","marksman"},
+        ensure_installed = { "sumneko_lua","clangd", "cmake", "pyright", "gopls","jsonls","marksman","bashls"},
         automatic_installation = true,
     })
 end

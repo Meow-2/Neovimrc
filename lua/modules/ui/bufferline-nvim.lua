387c9274fbfcffce88739b69f9fe0bac443dcb3e
N.O.Atomusk <orzkey@outlook.com>
2022-08-18 16:33:49 +0800

    fixed valid arg

--- lua/modules/ui/bufferline-nvim.lua
+++ lua/modules/ui/bufferline-nvim.lua
@@ -1,25 +1,13 @@
 return function()
-    --  require('bufferline').setup({
-    --    options = {
-    --      modified_icon = '✥',
-    --      buffer_close_icon = '',
-    --      always_show_bufferline = false,
-    --    },
-    --  })
-    --
     require('bufferline').setup({
         options = {
-            --        numbers = function(opts)
-            --                return string.format('%s.', opts.ordinal)
-            --              end,
-            --modified_icon = '✥',
             offsets = {
                 { filetype = 'NvimTree', text = ' File Explorer', text_align = 'center' },
             },
             diagnostics = false,
             buffer_close_icon = '',
             separator_style = 'thin',
-            always_show_bufferline = 'true',
+            always_show_bufferline = true,
             show_close_icon = false,
             right_mouse_command = 'vertical sbuffer %d',
         },

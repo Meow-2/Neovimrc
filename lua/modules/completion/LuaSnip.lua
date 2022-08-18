7b6b9141938dfdf10364057f898b4613e7aa5a37
N.O.Atomusk <orzkey@outlook.com>
2022-08-19 02:26:37 +0800

    fix snip syntax

--- lua/modules/completion/LuaSnip.lua
+++ lua/modules/completion/LuaSnip.lua
@@ -19,7 +19,7 @@ return function()
     require('luasnip.loaders.from_snipmate').lazy_load({
         paths = { './snippets/' },
     })
-    -- require('luasnip.loaders.from_vscode').lazy_load()
+    require('luasnip.loaders.from_vscode').lazy_load()
     require('luasnip.loaders.from_vscode').lazy_load({
         paths = { './snippets/' },
     })

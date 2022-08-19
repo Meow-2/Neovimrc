78b940159fb5049e4e4ce2a6c02adc531b07b7d8
N.O.Atomusk <orzkey@outlook.com>
2022-08-20 05:04:01 +0800

    clean code

--- after/ftplugin/dap-repl.lua
+++ after/ftplugin/dap-repl.lua
@@ -1,13 +1,13 @@
 local dap_repl_bufid = vim.fn.bufnr('dap-repl')
 require('dap.ext.autocompl').attach()
 
+print('Dap Has Started!')
 local dap_repl_group = vim.api.nvim_create_augroup('Dap_repl_complete', { clear = true })
 
 vim.api.nvim_create_autocmd('CursorMovedI', {
     group = dap_repl_group,
     buffer = dap_repl_bufid,
     callback = function()
-        print(' bufferid = ' .. dap_repl_bufid)
         vim.api.nvim_feedkeys(
             vim.api.nvim_replace_termcodes('<C-x><C-o>', true, false, true),
             'm',
@@ -20,7 +20,7 @@ vim.api.nvim_create_autocmd({ 'BufWinLeave' }, {
     group = dap_repl_group,
     buffer = dap_repl_bufid,
     callback = function()
-        print('del autogroup' .. ' bufferid = ' .. dap_repl_bufid)
+        print('Dap Has Exited!')
         vim.api.nvim_del_augroup_by_name('Dap_repl_complete')
     end,
 })

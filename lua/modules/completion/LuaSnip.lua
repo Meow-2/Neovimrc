edbad98c3f1b939073c3b94b6f86996a3aefabe6
N.O.Atomusk <orzkey@outlook.com>
2022-08-19 17:26:56 +0800

    use snipmate as snippet

--- lua/modules/completion/LuaSnip.lua
+++ lua/modules/completion/LuaSnip.lua
@@ -16,11 +16,10 @@ return function()
     -- require('luasnip.loaders.from_lua').lazy_load({
     --     paths = vim.fn.stdpath('config') .. '/snippets',
     -- })
+    -- require('luasnip.loaders.from_vscode').lazy_load({
+    --     paths = { './snippets/' },
+    -- })
     require('luasnip.loaders.from_snipmate').lazy_load({
-        paths = { './snippets/' },
-    })
-    require('luasnip.loaders.from_vscode').lazy_load()
-    require('luasnip.loaders.from_vscode').lazy_load({
         paths = { './snippets/' },
     })
 end

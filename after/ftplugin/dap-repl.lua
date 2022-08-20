2954b6fa5940785d14db13e04b5fa44dc4770615
N.O.Atomusk <orzkey@outlook.com>
2022-08-21 02:12:25 +0800

    use tab to complete and cr to auto del redundant `

--- after/ftplugin/dap-repl.lua
+++ after/ftplugin/dap-repl.lua
@@ -3,7 +3,24 @@ require('dap.ext.autocompl').attach()
 
 print('Dap Has Started!')
 local dap_repl_group = vim.api.nvim_create_augroup('Dap_repl_complete', { clear = true })
-
+vim.keymap.set(
+    'i',
+    '<Tab>',
+    '<C-n>',
+    { noremap = true, silent = true, nowait = true, buffer = dap_repl_bufid }
+)
+vim.keymap.set(
+    'i',
+    '<S-Tab>',
+    '<C-p>',
+    { noremap = true, silent = true, nowait = true, buffer = dap_repl_bufid }
+)
+vim.keymap.set(
+    'i',
+    '<Cr>',
+    [[<Cmd>silent! s/``/`/g<Cr><Cr>]],
+    { silent = true, nowait = true, buffer = dap_repl_bufid }
+)
 vim.api.nvim_create_autocmd('CursorMovedI', {
     group = dap_repl_group,
     buffer = dap_repl_bufid,
@@ -21,6 +38,9 @@ vim.api.nvim_create_autocmd({ 'BufWinLeave' }, {
     buffer = dap_repl_bufid,
     callback = function()
         print('Dap Has Exited!')
+        vim.keymap.del('i', '<Tab>', { buffer = dap_repl_bufid })
+        vim.keymap.del('i', '<S-Tab>', { buffer = dap_repl_bufid })
+        vim.keymap.del('i', '<Cr>', { buffer = dap_repl_bufid })
         vim.api.nvim_del_augroup_by_name('Dap_repl_complete')
     end,
 })

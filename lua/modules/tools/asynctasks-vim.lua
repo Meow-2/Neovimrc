85e4da740f52e2f9c60cf53c0c6592a53d88374b
N.O.Atomusk <orzkey@outlook.com>
2022-09-03 17:25:09 +0800

    add root mark

--- lua/modules/tools/asynctasks-vim.lua
+++ lua/modules/tools/asynctasks-vim.lua
@@ -2,5 +2,5 @@ return function()
     vim.g.asyncrun_open = 12
     vim.g.asynctasks_term_rows = '12'
     vim.g.asynctasks_term_pos = 'bottom'
-    vim.g.asyncrun_rootmarks = { '.git', '.svn', '.root', '.project', '.hg' }
+    vim.g.asyncrun_rootmarks = { '.git', '.svn', '.root', '.project', '.hg', 'CMakeLists.txt' }
 end

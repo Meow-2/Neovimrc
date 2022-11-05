c78136d47275d9e33d81b2c63143ae6dbb54bb7f
N.O.Atomusk <orzkey@outlook.com>
2022-11-05 22:21:20 +0800

    change chrome to edge

--- lua/modules/tools/markdown-preview-nvim.lua
+++ lua/modules/tools/markdown-preview-nvim.lua
@@ -1,6 +1,7 @@
 return function()
     vim.g.mkdp_echo_preview_url = 1
-    vim.g.mkdp_browser = 'google-chrome-stable'
+    -- vim.g.mkdp_browser = 'google-chrome-stable'
+    vim.g.mkdp_browser = 'microsoft-edge-stable'
     if vim.g.remoteSession then
         vim.g.mkdp_open_to_the_world = 1
         -- change to you vps or vm ip

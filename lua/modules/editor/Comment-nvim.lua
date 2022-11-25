cad49e6a3c91819c55392c42bd5a619a1c3cf19f
N.O.Atomusk <orzkey@outlook.com>
2022-11-26 03:29:24 +0800

    fix comment keybinding

--- lua/modules/editor/Comment-nvim.lua
+++ lua/modules/editor/Comment-nvim.lua
@@ -9,14 +9,14 @@ return function()
         ---LHS of toggle mappings in NORMAL mode
         toggler = {
             ---Line-comment toggle keymap
-            line = '<C-_>',
+            line = '<C-/>',
             ---Block-comment toggle keymap
             block = '<C-b>',
         },
         ---LHS of operator-pending mappings in NORMAL and VISUAL mode
         opleader = {
             ---Line-comment keymap
-            line = '<C-_>',
+            line = '<C-/>',
             ---Block-comment keymap
             block = '<C-b>',
         },

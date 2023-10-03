f3b204709af03a13237937f9fcd30481d89ea98f
N.O.Atomusk <orzkey@outlook.com>
2023-10-04 07:11:18 +0800

    update indent-blankline-nvim config to version 3

--- lua/modules/dress/indent-blankline-nvim.lua
+++ lua/modules/dress/indent-blankline-nvim.lua
@@ -1,28 +1,31 @@
 return function()
     -- vim.g.indent_blankline_char = "▏"
     -- vim.g.indent_blankline_context_char = "▏"
-    require('indent_blankline').setup({
+    require('ibl').setup({
         -- for example, context is off by default, use this to turn it on
-        show_current_context = true,
-        buftype_exclude = { 'terminal' },
-        filetype_exclude = {
-            'lazy',
-            'dashboard',
-            'NvimTree',
-            'mason',
-            'lspsagaoutline',
-            'lspsagafinder',
-            'packer',
-            'Outline',
-            'startuptime',
-            'dap-repl',
-            'dapui_breakpoints',
-            'dapui_watches',
-            'dapui_stacks',
-            'dapui_scopes',
-            'noice',
-            'nvimgdb',
-            'qf',
+        indent = { char = '│' },
+        scope = { enabled = true, show_start = false, show_end = false },
+        exclude = {
+            buftypes = { 'terminal' },
+            filetypes = {
+                'lazy',
+                'dashboard',
+                'NvimTree',
+                'mason',
+                'lspsagaoutline',
+                'lspsagafinder',
+                'packer',
+                'Outline',
+                'startuptime',
+                'dap-repl',
+                'dapui_breakpoints',
+                'dapui_watches',
+                'dapui_stacks',
+                'dapui_scopes',
+                'noice',
+                'nvimgdb',
+                'qf',
+            },
         },
     })
 end
--- lua/modules/dress/package.lua
+++ lua/modules/dress/package.lua
@@ -50,6 +50,7 @@ package({
     'lukas-reineke/indent-blankline.nvim',
     -- event = { 'BufNewFile', 'BufRead' },
     event = { 'VeryLazy' },
+    main = "ibl",
     dependencies = { 'nvim-treesitter' },
     config = conf('indent-blankline.nvim'),
 })

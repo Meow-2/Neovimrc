516d2020b6506c108b0681e506c779d7837bfaa5
N.O.Atomusk <orzkey@outlook.com>
2023-10-14 19:35:54 +0800

    fix nerd font of dashboard

--- lua/modules/dress/dashboard-nvim.lua
+++ lua/modules/dress/dashboard-nvim.lua
@@ -10,7 +10,7 @@ return function()
             },
             shortcut = {
                 {
-                    icon = ' ',
+                    icon = '󰊳 ',
                     icon_hl = '@variable',
                     desc = 'Update',
                     group = '@property',

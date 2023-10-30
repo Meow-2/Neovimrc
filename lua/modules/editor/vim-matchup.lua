9d40dfc6bd5158eb681078f232b725b1d752210b
N.O.Atomusk <orzkey@outlook.com>
2023-10-30 11:46:36 +0800

    disable vim-matchup highlight in html

--- lua/modules/editor/vim-matchup.lua
+++ lua/modules/editor/vim-matchup.lua
@@ -2,6 +2,14 @@ return function()
     -- vim.g.matchup_motion_enabled = 0
     -- vim.g.matchup_text_obj_enabled = 0
     vim.g.matchup_matchparen_offscreen = {}
-    -- method = 'popup',
-    -- }
+    vim.g.matchup_delim_stopline = 400
+    vim.g.matchup_matchparen_stopline = 400
+
+    vim.api.nvim_create_autocmd({ 'FileType' }, {
+        pattern = 'html',
+        callback = function()
+            vim.g.matchup_matchparen_enabled = 0
+            -- vim.g.matchup_matchparen_fallback = 0
+        end,
+    })
 end

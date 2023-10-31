74afb9d03a6d612318919b2a8a1e38d0c0d823ad
N.O.Atomusk <orzkey@outlook.com>
2023-10-31 23:37:25 +0800

    add support for large file

--- init.lua
+++ init.lua
@@ -1,4 +1,7 @@
-if vim.g.vscode then
+local filesize = vim.fn.getfsize(vim.fn.expand('%'))
+if filesize > 750000 then
+    vim.cmd([[luafile ~/.config/nvim/static/largefile.lua]])
+elseif vim.g.vscode then
     vim.cmd([[source ~/.config/nvim/static/vscode.vim]])
 else
     require('core')
